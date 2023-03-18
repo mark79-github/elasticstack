985335ef565a5c1269aa0e47426f53eebc106cf7
Martin Dimitrov <52877024+mark79-github@users.noreply.github.com>
2023-03-18 10:58:48 +0200

    Create setup-hosts.sh

---
+++ setup-hosts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "*** Add hosts ..."
+echo "192.168.34.201 server.martin.bg server" >> /etc/hosts
+echo "192.168.34.202 centos.martin.bg centos" >> /etc/hosts
+echo "192.168.34.203 ubuntu.martin.bg ubuntu" >> /etc/hosts
