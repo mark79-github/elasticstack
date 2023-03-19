59897e9a406a7c94012538013faffb16f7191477
Martin Dimitrov <52877024+mark79-github@users.noreply.github.com>
2023-03-19 19:11:00 +0200

    Update setup-update.sh

--- setup-update.sh
+++ setup-update.sh
@@ -2,7 +2,7 @@
 
 echo "*** Updating repository info ..."
 if [[ $(hostname -f) == *"ubuntu"* ]]; then
-    sudo apt-get update -y
+    sudo apt-get update -y && sudo apt-get upgrade -y
 else
-    sudo dnf update -y
+    sudo dnf update -y && sudo dnf upgrade -y
 fi
